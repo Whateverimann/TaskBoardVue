988b171aeea0bc4b868dfbcf9d9ec2ac67fcf018
Anna <wrobel.anne@outlook.com>
2020-10-13 02:02:08 +0200

    change

--- VueTaskBoard/src/vue.config.js
+++ VueTaskBoard/src/vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/VueTaskBoard/vue-trello/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/VueTaskBoard/' : '/'
 }
